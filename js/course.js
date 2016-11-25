ec5630198c59497b26134ae8ff99bcf45e47f5db
chaisir <18610360630@163.com>
2016-11-25 16:24:56 +0800

    解决页面跳转问题

--- js/course.js
+++ js/course.js
@@ -2,5 +2,8 @@
  * Created by Administrator on 2016/11/25 0025.
  */
 $(function(){
-    $("")
+    $("div.row ul li").click(function(e){
+        e.preventDefault();
+        location.reload("true");
+    })
 })
