05e5464802586b203571f42f98559e16462302c2
chaisir <18610360630@163.com>
2016-11-30 17:45:05 +0800

    选课部分之间切换

--- js/couseBrief.js
+++ js/couseBrief.js
@@ -13,7 +13,39 @@ $(function(){
         $(this).parent().siblings().css("background","url('image/circle_2_03.png') no-repeat 5px 3px");
     })
 //  课程目录下的切换功能
-    $(".con_2 div").click(function(){
-        $(this).next().slideToggle(2000).siblings("p").slideUp(2000);
+    $(".con_2>div").click(function(){
+        if($(this).children("span.arrow").hasClass("active")){
+                $(this).children("span.arrow").removeClass("active")
+                $(this).siblings().children("span.arrow").removeClass("active");
+        }else{
+                $(this).children("span.arrow").addClass("active");
+                $(this).siblings().children("span.arrow").removeClass("active");
+        }
+        $(this).next().slideToggle(1000).siblings("h5").slideUp(1000);
+    })
+    $("ul.msg li").click(function(){
+        $(this).children().slideToggle();
+        $(this).siblings().children().slideUp();
+    })
+
+
+//    简介/目录/资料之间的相关切换
+    $(".cen_2").click(function(e){
+        e.preventDefault();
+        $(".con_2").show();
+        $(".con_3").hide();
+        $(".con_1").hide();
+    })
+    $(".cen_1").click(function(e){
+        e.preventDefault();
+        $(".con_1").show();
+        $(".con_2").hide();
+        $(".con_3").hide();
+    })
+    $(".cen_3").click(function(e){
+        e.preventDefault();
+        $(".con_3").show();
+        $(".con_1").hide();
+        $(".con_2").hide();
     })
 })
