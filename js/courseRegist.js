c77127e945a7f1f95a0d3a46c0610c64d21ef733
chaisir <18610360630@163.com>
2016-12-01 14:17:11 +0800

    报名页面/确认信息/提交审核之间的切换

--- js/courseRegist.js
+++ js/courseRegist.js
@@ -1,3 +1,40 @@
 /**
  * Created by Administrator on 2016/11/28 0028.
  */
+$(function(){
+    $("footer").load("footer/footer.html");
+
+    $("div.page_1 .info_2").click(function(){
+        $(".page_1").hide();
+        $(".page_2").show();
+    })
+
+    $("div.page_2 span.p_2").click(function(){
+        $(".page_2").hide();
+        $(".page_1 .page_body").hide();
+        $(".page_1").show();
+        $(".page_1 .pay").show();
+        $("#entry img").attr("src","image/wait_2.png");
+    })
+   $("div.pay span.p_1").click(function(){
+       $("div.page_1").hide();
+       $(".page_2").show();
+       $("#entry img").attr("src","image/wait_1.png");
+   })
+    $("div.pay span.p_2").click(function(){
+        $("div.pay").hide();
+        $("div.check_1").show();
+        $("#entry img").attr("src","image/wait_3.png");
+    })
+    $("div.check_1 span").click(function(){
+        $("div.check_1").hide();
+        $("div.check_2").show();
+    })
+    $("div.page_2 span.p_1").click(function(){
+        $("div.pay").hide();
+        $(".page_2").hide();
+        $(".page_1").show();
+        $("div.page_body").show();
+
+    })
+})
