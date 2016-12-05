3936efdac42d04fe27503ac1064da950925a8ed8
chaisir <18610360630@163.com>
2016-12-05 22:23:24 +0800

    替换之前轮播华东效果

--- js/learnCenter.js
+++ js/learnCenter.js
@@ -7,8 +7,7 @@ $(function(){
 
     $("div.con_2 li p").hover(function(){
             $(this).css("color","#73B2E7");
-        },
-        function(){
+        },function(){
             $(this).css("color","#686868")
         });
 
@@ -16,6 +15,21 @@ $(function(){
     jQuery(".picScroll-left")
         .slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",vis:3});
 
+//    ÏÂÀ­ÇÐ»»
+    $(".con_2>div").click(function(){
+        if($(this).children("span.arrow").hasClass("active")){
+            $(this).children("span.arrow").removeClass("active")
+            $(this).siblings().children("span.arrow").removeClass("active");
+        }else{
+            $(this).children("span.arrow").addClass("active");
+            $(this).siblings().children("span.arrow").removeClass("active");
+        }
+        $(this).next().slideToggle(1000).siblings("h5").slideUp(1000);
+    })
+    $("ul.msg li").click(function(){
+        $(this).children("p").slideToggle();
+        $(this).siblings().children("p").slideUp();
+    })
 
 
 
