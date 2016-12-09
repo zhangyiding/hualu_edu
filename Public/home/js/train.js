d927b556e4c7b92d46af574cdb687fb176e1e2e4
Liu jinshuai <ljs_0829@qq.com>
2016-12-09 14:48:26 +0800

    页面调优

--- Public/home/js/train.js
+++ Public/home/js/train.js
@@ -7,21 +7,33 @@ $(function(){
     $("#page ul li a").mouseover(function(){
         $("#page ul li.active").removeClass("active");
     })
-    $(".banner ul").on("click","li",function(){
-        $(this).addClass("active").siblings(".active").removeClass("active");
-    })
-    $(".banner li.country a").click(function(e){
-        $("div.tab_1").show();
-        $("div.tab_2").hide();
-    })
-    $(".banner li.busy").click(function(e){
-        $("div.tab_2").show();
-        $("div.tab_1").hide();
-    })
+
+
 //   ��ҳ�л�����
     $(".nav li a").click(function(e){
         //e.preventDefault();
         $(this).parent().addClass("active").siblings(".active").removeClass("active");
-    })
+    });
 
-})
+
+    //将当前url中参数转换为数组
+    function parseQueryString(url){
+        var json = {};
+        var arr = url.substr(url.indexOf('?') + 1).split('&');
+        arr.forEach(function(item) {
+            var tmp = item.split('=');
+            json[tmp[0]] = tmp[1];
+        })
+        return json;
+    }
+    var url =window.location.href;
+    var json = parseQueryString(url);
+
+    if(json.news_type == 2){
+        $('div.banner ul li.busy').addClass('active');
+    }else {
+        $('div.banner ul li.country').addClass('active');
+    }
+
+
+});
