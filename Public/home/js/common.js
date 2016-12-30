9d710f9881eea7a805e1a838428e83e1e91ebad3
Liu jinshuai <ljs_0829@qq.com>
2016-12-30 15:44:54 +0800

    fix: 修改js访问的url

--- Public/home/js/common.js
+++ Public/home/js/common.js
@@ -2,9 +2,7 @@
  * Created by Administrator on 2016/11/25 0025.
  */
 $(function(){
-    window.BASE_URL = '//edu.hl.com';
-
-
+    window.BASE_URL = '//test.tcsasac.com';
 
     $("#footer").load("/index/footer");
     $("#header").load("/index/header");
