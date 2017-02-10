7eaa07f2781f560025d3a9f31368bc5c147063c2
Liu jinshuai <ljs_0829@qq.com>
2017-02-10 16:44:14 +0800

    fix: 修改前台部分样式问题

--- Public/home/js/index.js
+++ Public/home/js/index.js
@@ -57,12 +57,16 @@ $(function() {
 
 //����ί��ѵ�л�
     $("span.train_1").click(function(){
+        $("span.train_2").css("color","#4F4F4F");
         $("ul.new_1").fadeIn();
         $("ul.new_2").fadeOut();
         $(this).css("background-image","url('Public/home/image/rightbar1.png')");
+        $(this).css("color","#FFFFFF");
         $("span.train_2").css("backgroundImage","url('Public/home/image/rightbar2.png')");
     })
     $("span.train_2").click(function(){
+        $("span.train_1").css("color","#4F4F4F");
+        $(this).css("color","#FFFFFF");
         $("ul.new_1").fadeOut();
         $("ul.new_2").fadeIn();
         $(this).css("background-image","url('Public/home/image/rightbar1.png')");
